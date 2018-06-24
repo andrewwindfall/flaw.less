66cb621941016b7cc7f00f26d9dca1e5b154d5a8
andrewwindfall <andrew@windfallstudio.com>
2018-06-23 23:46:27 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+  <title>Windfall, Inc. | Missoula, MT Advertising Agency </title>
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <!-- Facebook Metadata /-->
+	<meta property="fb:page_id" content="" />
+	<meta property="og:image" content="" />
+	<meta property="og:description" content=""/>
+	<meta property="og:title" content=""/>
+
+	<!-- Google+ Metadata /-->
+	<meta itemprop="name" content="">
+	<meta itemprop="description" content="">
+	<meta itemprop="image" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="/assets/css/app.css">
+	<script src="/assets/js/jquery-2.0.2.min.js"></script>
+  </head>
+
+  <body style="background-color:white;">
+		<div class="row no-gutter half" style="background-color:white;">
+			<div class="column dark-bg box white">
+				<h1>This is heading 1</h1>
+				<h2>This is heading 2</h2>
+				<h3>This is heading 3</h3>
+				<h4>This is heading 4</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h5>This is heading 5</h5>
+				<h6>This is heading 6</h6>
+				<div class="row half">
+					<div class="column teal-bg">
+						Hey
+					</div>
+					<div class="column" style="background-color:white; color:#000;">
+						<div class="row one">
+							<div class="column">
+								<h1>This is heading 1</h1>
+								<h2>This is heading 2</h2>
+								<h3>This is heading 3</h3>
+								<h4>This is heading 4</h4>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+								<h5>This is heading 5</h5>
+								<h6>This is heading 6</h6>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div class="column light-bg box-top">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h2 class="expander" data-expand="#expand-me">Link Title</h2>
+				<div id="expand-me" class="expandable">
+				  <p>Content</p>
+					<li class="modalbtn" data-modal="#modalname"><a>Link Title</a></li>
+				</div>
+			</div>
+
+		</div>
+		<div class="row six dark-bg">
+			<div class="two columns box red-bg">
+				hey
+			</div>
+			<div class="column box teal-bg">
+				hey
+			</div>
+			<div class="column box teal-bg">
+				hey
+			</div>
+			<div class="column box teal-bg">
+				hey
+			</div>
+			<div class="column box teal-bg">
+				hey
+			</div>
+		</div>
+		<div class="row six dark-bg">
+			<div class="columns box dblue-bg">
+				hey
+			</div>
+			<div class="column box red-bg">
+				hey
+			</div>
+			<div class="column box teal-bg">
+				hey
+			</div>
+			<div class="column push-two box teal-bg">
+				hey
+			</div>
+
+		</div>
+
+
+
+
+
+
+		<div id="modalname" class="modal" style="display: none;">
+      <div class="modal-content">
+        <div class="modal-header">
+          <span class="close">×</span>
+          <h2>Modal Header Name</h2>
+        </div>
+        <div class="modal-body">
+          <p>Modal Content Goes Here</p>
+        </div>
+      </div>
+
+    </div>
+
+		<script src="/assets/js/flawless.js"></script>
+
+  </body>
+  </html>
